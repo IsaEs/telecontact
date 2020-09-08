4520f19ff22b6bd5a04cab2ce7f9e2cc0396e4ab
IsaEs <merhaba@isaes.com.tr>
2020-09-08 22:19:54 +0300

    feat: authorize and authenticate user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
-'use strict'
+const debug = require('debug')('app:models:user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: false, },
@@ -12,6 +15,45 @@ module.exports = (sequelize, DataTypes) => {
   user.associate = function () {
     // associations can be defined here
   }
+
+
+  user.authenticate = async function (where, password) {
+    debug('Where', where)
+    let attributes = { exclude: ['createdAt', 'updatedAt'] }
+    const current_user = await user.findOne({ where, attributes })
+    debug('Authenticate: ', current_user)
+    if (current_user !== null) {
+      if (bcrypt.compareSync(password, current_user.password_hash)) {
+        debug('Password correct!')
+        return current_user.authorize()
+      } else {
+        return { status: 401, error: 'Invalid username or password' }
+      }
+    } else {
+      return { status: 404, error: 'No such a user!' }
+    }
+  }
+
+
+  user.prototype.authorize = async function () {
+    debug('Authenticate...', process.env.JWT_SECRET_KEY)
+    const user = this.dataValues
+    delete user['password_hash']
+    delete user['isDeleted']
+    debug('User :', user)
+    const access_token = 'user' //AccessToken
+    const token_type = 'jwt' //Type
+    const expiresIn = 1000 * 60 * 60 * 24 * 15 //15 day
+    const expiresAt = new Date(Date.now() + expiresIn) //Experies At
+    const token = await jwt.sign(
+      { access_token, user, token_type, expiresIn, expiresAt },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn }
+    )
+    debug('Token', token)
+    return { status: 200, token }
+  }
+
   return user
 }
 
