57f98c755ffd7b1770c830aaeee2ff9a7d929d80
IsaEs <merhaba@isaes.com.tr>
2020-09-09 19:23:43 +0300

    feat: preferences model

---
+++ migrations/20200909115046-create-preference.js
@@ -0,0 +1,44 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('preferences', {
+      sendMail: {
+        type: Sequelize.BOOLEAN, defaultValue: false
+      },
+      saveMessage: {
+        type: Sequelize.BOOLEAN, defaultValue: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      formId: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        references: {
+          model: 'websites',
+          key: 'formId',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('preferences')
+  }
+}
