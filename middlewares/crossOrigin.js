3f27ade0a39e15188ffc552b5504332dcd13ce5f
IsaEs <merhaba@isaes.com.tr>
2020-09-09 03:48:27 +0300

    improvement: move crossOrigin to middlewares

---
+++ middlewares/crossOrigin.js
@@ -0,0 +1,16 @@
+// Allow Cross Origin
+let allowCors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Authorization,Content-type,Accept,X-Access-Token,X-Key,Device-Id'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
+    next()
+  }
+}
+
+module.exports = allowCors
