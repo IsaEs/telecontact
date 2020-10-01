77c950fdaf70e60f34761f8930546e8db4a2f096
IsaEs <merhaba@isaes.com.tr>
2020-10-01 23:46:48 +0300

    feat: telegram weblogin

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,15 @@
 let express = require('express')
 let router = express.Router()
 let db = require('../models/index')
+const { createHash, createHmac } = require('crypto')
 const debug = require('debug')('app:routes:signin')
 
 
 let user_signin = async (req, res) => {
   debug(req.body)
+  if(req.body.type==='telegram'){
+    return telegram_login(req,res)
+  }
   if (!req.body.email && !req.body.username) {
     res.status(500).send({ error: 'You have to set the email or username.' })
     return
@@ -36,6 +40,35 @@ let user_signin = async (req, res) => {
   }
 }
 
+
+function telegram_login(req,res){
+  const secret = createHash('sha256')
+    .update(process.env.TELEGRAM_TOKEN)
+    .digest()
+
+  if (checkSignature(req.body.user)){
+    if(req.user===403){
+      //TODO sign up send password
+    } else{
+      //Check and assoc with current user
+    }
+    res.status(200).json({message: 'Data From telegram'})
+  }else {
+    res.status(400).json({message: 'Zoort'})
+  }
+
+  function checkSignature ({ hash, ...data }) {
+    debug(data)
+    const checkString = Object.keys(data)
+      .sort()
+      .map(k => (`${k}=${data[k]}`))
+      .join('\n')
+    const hmac = createHmac('sha256', secret)
+      .update(checkString)
+      .digest('hex')
+    return hmac === hash
+  }
+}
 
 router.post('/signin', user_signin)
 
