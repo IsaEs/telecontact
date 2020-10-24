1b1ba6fc66e6424e0b2197000acb8814cb2ed067
IsaEs <merhaba@isaes.com.tr>
2020-10-24 13:32:25 +0300

    test: add example.test.js

---
+++ test/example.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const chai = require('chai')
+const expect = chai.expect
+
+describe('test', () => {
+  it('"554"must be equal 554 ', ()=> {
+    expect('554'==554).to.be.true
+  })
+
+})
