45e18fdeb6642b6c275236459fd6fb4907358856
IsaEs <merhaba@isaes.com.tr>
2020-10-30 23:33:05 +0300

    feat: change to  node-fetch due to deprecation

--- test/api.js
+++ test/api.js
@@ -1,12 +1,11 @@
 let apiUrl ='http://localhost:5050/api/v1/'
 
-let url = (str)=> {return {url : apiUrl + str}}
+let url = (str)=> {return apiUrl + str}
 
 let api = {
   healtcheck : url('healtcheck'), //GET
-  form: url('form'), // POST
-  user: url('user/forms'), //GET, DEL
-  addForm: url('user/form/add'), // PUT
+  submission: url('form'), // POST
+  user: url('user/forms'), // CRUD
   updateEmail: url('user/email'), // PUT
   preferences: url('user/preferences'), // GET, PUT
   profile: url('user/profile'),
--- test/healtcheck.test.js
+++ test/healtcheck.test.js
@@ -1,18 +1,17 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 var should = require('should')
-var request = require('request')
+const fetch = require('node-fetch')
 var chai = require('chai')
 var expect = chai.expect
 let API = require('./api')
 
 describe('Telecontact.me API test',()=>{
-  it('Should return 100 cards max',(done) =>{
-    request.get(API.healtcheck , (error, response, _body) =>{
-      var body = API.toJson(_body)
-      expect(response.statusCode).to.equal(200)
-      body.should.have.property('date') 
-      done()
-    })
+  it('Should return server date', async () =>{
+    const response = await  fetch(API.healtcheck)
+    const body = await response.json()
+    console.log(response.status)
+    expect(response.status).to.equal(200)
+    body.should.have.property('date') 
   })
 })
